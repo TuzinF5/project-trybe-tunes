8c1952a9c5febda5f24c7162603f46386c5ed254
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-19 01:03:53 -0300

    Fazendo verificação do campo input

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loginNameInput: '',
+      disableButton: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => this.checkInputSize());
+  }
+
+  checkInputSize() {
+    const minimumNumber = 3;
+    const { loginNameInput } = this.state;
+    if (loginNameInput.length >= minimumNumber) {
+      this.setState({
+        disableButton: false,
+      });
+    }
+  }
+
   render() {
+    const valueState = this.state;
     return (
       <div data-testid="page-login">
         <form>
           <input
             type="text"
-            name=""
+            name="loginNameInput"
             id="login-name-input"
+            value={ valueState.loginNameInput }
+            onChange={ this.handleChange }
             data-testid="login-name-input"
           />
-          <button type="button" data-testid="login-submit-button">Entrar</button>
+          <button
+            type="button"
+            disabled={ valueState.disableButton }
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
