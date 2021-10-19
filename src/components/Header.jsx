bfe48be8d33338ca437a0b93508403c0dcb79351
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-19 03:46:22 -0300

    Requisito 3 concluído

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { userName } = this.state;
+    if (!userName.length) {
+      getUser().then((object) => this.setState({
+        userName: object.name,
+        loading: false,
+      }));
+    }
+  }
+
   render() {
+    const { userName, loading } = this.state;
+
+    if (loading) return <Loading />;
+
     return (
-      <header data-testid="header-component">Header</header>
+      <header data-testid="header-component">
+        <p data-testid="header-user-name">{userName}</p>
+      </header>
     );
   }
 }
