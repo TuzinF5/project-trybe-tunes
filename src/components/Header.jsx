4cdebd303494ddb07904b58a2c9349499fb0e3aa
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-19 04:02:15 -0300

    Renderizando Link para url /profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,6 +32,7 @@ class Header extends React.Component {
         <p data-testid="header-user-name">{userName}</p>
         <Link to="/search" data-testid="link-to-search">Search</Link>
         <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
