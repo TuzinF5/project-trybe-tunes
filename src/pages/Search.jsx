e605f90b675f9b00f85aa5df4daee212cd341415
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-19 04:14:57 -0300

    Criando funções para desabilitar/habilitar button

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,18 +2,51 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nameInput: '',
+      disableButton: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => this.checkInputSize());
+  }
+
+  checkInputSize() {
+    const minimumNumber = 2;
+    const { nameInput } = this.state;
+    if (nameInput.length >= minimumNumber) {
+      this.setState({
+        disableButton: false,
+      });
+    }
+  }
+
   render() {
+    const valueState = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
         <form>
           <input
             type="text"
-            name=""
+            name="nameInput"
             id="search-artist-input"
             data-testid="search-artist-input"
+            value={ valueState.nameInput }
+            onChange={ this.handleChange }
           />
-          <button type="submit" data-testid="search-artist-button">
+          <button
+            type="submit"
+            disabled={ valueState.disableButton }
+            data-testid="search-artist-button"
+          >
             Pesquisar
           </button>
         </form>
