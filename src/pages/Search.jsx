43c08a15b083485d920c9f3538639b5f3b3c8cb0
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-19 18:14:39 -0300

    Mostrando os álbuns na tela

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -12,6 +12,7 @@ class Search extends React.Component {
       disableButton: true,
       loading: false,
       artist: '',
+      artistList: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -42,8 +43,8 @@ class Search extends React.Component {
       artist: nameInput,
       nameInput: '',
       loading: false,
+      artistList: objectArtist,
     });
-    console.log(objectArtist);
   }
 
   render() {
@@ -74,10 +75,14 @@ class Search extends React.Component {
         ) : (
           <Loading />
         )}
-        {
-          valueState.artist.length > 0
-          && <p>{`Resultado de álbuns de: ${valueState.artist}`}</p>
-        }
+        {valueState.artist.length > 0 && (
+          <div>
+            <p>{`Resultado de álbuns de: ${valueState.artist}`}</p>
+            {valueState.artistList.map((element, index) => (
+              <p key={ index }>{element.collectionName}</p>
+            ))}
+          </div>
+        )}
       </div>
     );
   }
