806600d0e1ee50469ae8195d20fa8a17c480377c
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-19 00:23:05 -0300

    Criando form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form>
+          <input
+            type="text"
+            name=""
+            id="login-name-input"
+            data-testid="login-name-input"
+          />
+          <button type="button" data-testid="login-submit-button">Entrar</button>
+        </form>
+      </div>
     );
   }
 }
