4e31ce1aa95a893126c7b20dbf9115069006ad0c
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-20 02:23:47 -0300

    Criando input do tipo checkbox

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class MusicCard extends React.Component {
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { trackName, previewUrl, trackId } = this.props;
     return (
       <div>
         <h3>{trackName}</h3>
@@ -13,6 +13,15 @@ class MusicCard extends React.Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="checkbox-music">
+          Favorita
+          <input
+            type="checkbox"
+            name=""
+            id="checkbox-music"
+            data-testid={ `checkbox-music-${trackId}` }
+          />
+        </label>
       </div>
     );
   }
@@ -21,6 +30,7 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.string.isRequired,
 };
 
 export default MusicCard;
