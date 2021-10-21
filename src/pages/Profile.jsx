1e6531d047c796b2fbcb91658a71f0a4cdde3b2c
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-21 14:17:17 -0300

    Renderizando as informações do usuario

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      user: '',
+    };
+
+    this.userInformation = this.userInformation.bind(this);
+  }
+
+  componentDidMount() {
+    this.userInformation();
+  }
+
+  async userInformation() {
+    this.setState({
+      loading: true,
+    });
+    const user = await getUser();
+    this.setState({
+      loading: false,
+      user,
+    });
+  }
+
   render() {
+    const { loading, user } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
+        {
+          loading && <Loading />
+        }
+        <div>
+          <p>{user.name}</p>
+          <p>{user.description}</p>
+          <p>{user.email}</p>
+          <img src={ user.image } alt={ user.name } data-testid="profile-image" />
+        </div>
       </div>
     );
   }
