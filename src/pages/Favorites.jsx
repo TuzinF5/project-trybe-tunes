e499d8f2197bb7a09f579d170aa252911b19686e
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-21 12:23:22 -0300

    Chamando a função getFavoriteSongs para recuperar as músicas favoritas

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.getFavorites = this.getFavorites.bind(this);
+  }
+
+  componentDidMount() {
+    this.getFavorites();
+  }
+
+  async getFavorites() {
+    await getFavoriteSongs();
+  }
+
   render() {
     return (
       <div data-testid="page-favorites">
