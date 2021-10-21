cfb5c351aece83fe3cdc5471198b489036e2ae24
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-21 14:21:33 -0300

    Criando Link para a rota de edição de perfil com o texto Editar perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
@@ -45,6 +46,7 @@ class Profile extends React.Component {
           <p>{user.email}</p>
           <img src={ user.image } alt={ user.name } data-testid="profile-image" />
         </div>
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
     );
   }
